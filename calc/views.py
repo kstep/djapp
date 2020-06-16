c89ff9172f7865bbc555bcf566a3b6b4762ed5b6
Konstantin Stepanov <me@kstep.me>
2020-06-16 15:12:11 +0300

    rename template subdir to match app name

--- calc/views.py
+++ calc/views.py
@@ -6,12 +6,12 @@ import math
 # Create your views here.
 
 class IndexView(TemplateView):
-    template_name = 'app/index.html'
+    template_name = 'calc/index.html'
 
     def get_context_data(self):
         return {'PI': math.PI}
 
 
 class HistoryView(ListView):
-    template_name = 'app/history.html'
+    template_name = 'calc/history.html'
     model = models.Expr
